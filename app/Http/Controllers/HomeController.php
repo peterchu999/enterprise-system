e485177127f85f3cd2c023376b39e9f038de9dce
peterchu999 <peter.andrew987@gmail.com>
2020-12-21 13:03:13 +0700

    refactor index min

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
+use App\Mail\BackupMail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -32,7 +38,23 @@ class HomeController extends Controller
     }
 
     public function first() {
-        Artisan::call('automate');
-        return view('FAQ');
+        $time = Carbon::now('Asia/Jakarta');
+        $counter = DB::table('backup_counters')->where('id', 1)->first();
+        if ($counter != null) {
+            $diff = $time->diffInDays($counter->counter);
+            if($diff >= 4){
+                $file = new Filesystem;
+                $file->cleanDirectory('storage/app/backup');
+                Artisan::call('backup:run --only-db');
+                $files = Storage::files('backup')[0];
+                $files = storage_path().'\\app\\'.$files;
+                Mail::to("winston@wirasukses.com")->send(new BackupMail($files, $time->toDateString()));
+                Mail::to("peter.andrew987@gmail.com")->send(new BackupMail($files, $time->toDateString()));
+                DB::table('backup_counters')->where('id', 1)->update(['counter'=>$time]);
+            }
+        } else {
+            DB::table('backup_counters')->insert(['counter'=>$time]);
+        }
+        return redirect()->route('FAQ');
     }
 }
